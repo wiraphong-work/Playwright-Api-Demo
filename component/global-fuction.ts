3e9ebc5a6a5e27ca009c6f11baf41dd776be3748
wiraphong-work <wiraphong.ch.work@gmail.com>
2024-03-01 11:31:09 +0700

    Clear file global-fuctions

--- component/global-fuction.ts
+++ component/global-fuction.ts
@@ -1,7 +1,6 @@
-import { test, expect, Page } from '@playwright/test';
+import { expect } from '@playwright/test';
 
 export class MainPage {
-
     async checkKeyOfJson( responseBody: JSON ,arr: any[]) {
        await arr.forEach((val)=>{
         expect(responseBody).toHaveProperty(val);
