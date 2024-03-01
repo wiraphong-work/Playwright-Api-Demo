8eaa414f5764f7685ad6fc1298e4fddc79abc912
wiraphong-work <wiraphong.ch.work@gmail.com>
2024-03-01 14:02:55 +0700

    Create test Put/Patch/Delete/Logout

--- tests/api-testing.spec.ts
+++ tests/api-testing.spec.ts
@@ -29,6 +29,14 @@ test("login", async ({ request }) => {
   await component.checkKeyOfJson(responseBody, ["token"]);
 });
 
+
+test(`logout`, async ({ request }) => {
+  const response = await request.post(`/api/logout`);
+  await expect(response.status()).toBe(200);
+  const responseBody = await response.json();
+  await expect(responseBody).toBeTruthy();
+});
+
 test("Get all user", async ({ request }) => {
   const response = await request.get(`/api/users`);
   await expect(response.status()).toBe(200);
@@ -59,3 +67,38 @@ data.data_user.forEach(async (value, index) => {
     ]);
   });
 });
+
+test("Get user by id", async ({ request }) => {
+  const response = await request.get(`/api/users/1`);
+  await expect(response.status()).toBe(200);
+  const responseBody = await response.json();
+  await expect(responseBody).toBeTruthy();
+  await component.checkKeyOfJson(responseBody.data, [
+    "id",
+    "email",
+    "first_name",
+    "last_name",
+    "avatar",
+  ]);
+});
+
+  test(`Update user (PUT)`, async ({ request }) => {
+    const response = await request.put(`/api/users/1`);
+    await expect(response.status()).toBe(200);
+    const responseBody = await response.json();
+    await expect(responseBody).toBeTruthy();
+    await component.checkKeyOfJson(responseBody, ["updatedAt"]);
+  });
+
+  test(`Update user (PATCH)`, async ({ request }) => {
+    const response = await request.patch(`/api/users/1`);
+    await expect(response.status()).toBe(200);
+    const responseBody = await response.json();
+    await expect(responseBody).toBeTruthy();
+    await component.checkKeyOfJson(responseBody, ["updatedAt"]);
+  });
+
+  test(`Delete user`, async ({ request }) => {
+    const response = await request.delete(`/api/users/1`);
+    await expect(response.status()).toBe(204);
+  });
